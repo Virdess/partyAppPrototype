2241341ff0287484cd66ddd37ef2db0b5280f9eb
fkd <info@fkd.tweegee.ru>
2024-12-08 00:24:32 +0600

    init

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -11,13 +11,55 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-
-      <ExploreContainer name="Tab 1 page" />
+      <ion-item>
+        <ion-textarea v-model="message" label="Ваше сообщение" placeholder="Введите сообщение" ></ion-textarea>
+      </ion-item>
+      <ion-button @click="sendMessage" expand="block">Отправить</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonTextarea, IonItem, IonContent, IonButton } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
+import { io, Socket } from 'socket.io-client';
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+
+// Переменная для хранения сообщения
+const message = ref('');
+const socket: Socket = io('http://localhost:3000'); // Замените на URL вашего сервера
+
+// Отправка сообщения
+function sendMessage() {
+  if (message.value.trim()) {
+    socket.emit('message', { sender: 'User1', message: message.value });
+    message.value = ''; // Очистить поле ввода
+  }
+}
+
+// Подключение к сокету
+onMounted(() => {
+  socket.on('connect', () => {
+    console.log('Подключено к серверу');
+  });
+
+  socket.on('message', (data) => {
+    console.log('Получено сообщение:', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Отключено от сервера');
+  });
+});
+
+// Отключение при удалении компонента
+onBeforeUnmount(() => {
+  socket.disconnect();
+});
 </script>
+
+<style>
+textarea {
+  height: 50vh;
+}
+</style>
